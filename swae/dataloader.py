602277f73a16852835c9961a58343c886fad8c97
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2024-03-13 23:42:44 +0700

    hhh

--- swae/dataloader.py
+++ swae/dataloader.py
@@ -35,7 +35,7 @@ class MNISTDataLoader():
         instances_indices = torch.arange(len(self.train_dataset.targets))
         all_classes_indices = list()
         for i in range(self.num_classes):
-            class_index = instances_indices[self.train_dataset.targets == i]
+            class_index = instances_indices[self.train_dataset.targets == i].tolist()
             all_classes_indices.append(class_index)
 
         batch_sampler = SamplerFactory().get(
@@ -51,7 +51,7 @@ class MNISTDataLoader():
         instances_indices = torch.arange(len(self.test_dataset.targets))
         all_classes_indices = list()
         for i in range(self.num_classes):
-            class_index = instances_indices[self.test_dataset.targets == i]
+            class_index = instances_indices[self.test_dataset.targets == i].tolist()
             all_classes_indices.append(class_index)
 
         test_batch_sampler = SamplerFactory().get(
