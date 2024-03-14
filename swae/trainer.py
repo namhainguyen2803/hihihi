8c2fe87456d42e93f4af8a90e2667d5de85ba521
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2024-03-14 11:19:03 +0700

    hhh

--- swae/trainer.py
+++ swae/trainer.py
@@ -53,46 +53,63 @@ class SWAEBatchTrainer:
             x = x.to(self._device)
             y = y.to(self._device)
             recon_x, z_posterior = self.model_(x)
-            bce = F.binary_cross_entropy(recon_x, x)
 
             batch_size = x.size(0)
-            z_prior = self._distribution_fn(batch_size).to(self._device)
-
-            l1 = F.l1_loss(recon_x, x)
-
-            swd = sliced_wasserstein_distance(encoded_samples=z_posterior, distribution_samples=z_prior,
-                                              num_projections=self.num_projections_, p=self.p_,
-                                              device=self._device)
 
             list_z = list()
-            list_recon_loss = dict()
-            wasserstein_distances = dict()
-            for cls in range(self.num_classes):
 
-                recon_loss = F.binary_cross_entropy(recon_x[y == cls], x[y == cls])
+            list_recon_loss = dict()
+            list_swd = dict()
+            list_l1 = dict()
+
+            l1_loss, bce_loss, swd_loss = 0, 0, 0
+
+            for cls in range(self.num_classes):
+                recon_x_cls = recon_x[y == cls]
+                x_cls = x[y == cls]
 
                 z_cls = z_posterior[y == cls]
                 list_z.append(z_cls)
+
                 z_sample = self._distribution_fn(z_cls.shape[0]).to(self._device)
-                ws_dist = sliced_wasserstein_distance(encoded_samples=z_cls, distribution_samples=z_sample,
-                                                      num_projections=self.num_projections_, p=self.p_,
-                                                      device=self._device)
 
-                wasserstein_distances[cls] = ws_dist
+                recon_loss = F.binary_cross_entropy(recon_x_cls, x_cls)
+                latent_ws_dist = sliced_wasserstein_distance(encoded_samples=z_cls, distribution_samples=z_sample,
+                                                             num_projections=self.num_projections_, p=self.p_,
+                                                             device=self._device)
+
+                l1 = F.l1_loss(recon_x_cls, x_cls)
+
+                list_swd[cls] = latent_ws_dist
                 list_recon_loss[cls] = recon_loss
+                list_l1[cls] = l1
 
+                bce_loss += recon_loss
+                l1_loss += l1
+                swd_loss += latent_ws_dist
+
+                bce_loss /= batch_size
+                l1_loss /= batch_size
+                swd_loss /= batch_size
+
+            z_prior = self._distribution_fn(batch_size).to(self._device)
             fsw = FEFBSW_list(Xs=list_z, X=z_prior, device=self._device)
 
-            loss = bce + float(self.weight_fsw) * fsw + float(self.weight) * swd + l1
+            loss = bce_loss + float(self.weight_fsw) * fsw + float(self.weight) * swd_loss + l1_loss
 
             return {
+                'list_recon': list_recon_loss,
+                'list_swd': list_swd,
+                'list_l1': list_l1,
+
                 'loss': loss,
-                'bce': bce,
-                'FairSW': fsw,
-                'w2': swd,
+                'recon_loss':bce_loss,
+                'swd_loss': swd_loss,
+                'l1_loss': l1_loss,
+                'fsw_loss': fsw,
+
                 'encode': z_posterior,
-                'decode': recon_x,
-                'wasserstein_distances': wasserstein_distances
+                'decode': recon_x
             }
 
     def eval_on_batch(self, x, y):
