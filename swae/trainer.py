7750c96e3b0a8743d7b41704989e8321331b427a
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2024-03-14 18:13:22 +0700

    hhh

--- swae/trainer.py
+++ swae/trainer.py
@@ -32,7 +32,7 @@ class SWAEBatchTrainer:
         self.num_classes = num_classes
 
         self.weight = 5
-        self.weight_fsw = 0
+        self.weight_fsw = 5
 
     def train_on_batch(self, x, y):
         # reset gradients
