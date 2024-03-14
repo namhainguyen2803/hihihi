9a99a41770aa9efec29b59f61d487b0edb5cf518
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2024-03-14 18:06:14 +0700

    HHH

--- swae/models/mnist.py
+++ swae/models/mnist.py
@@ -133,3 +133,5 @@ class MNISTAutoencoder(nn.Module):
     def forward(self, x):
         z = self.encoder(x)
         return self.decoder(z), z
+    def generate(self, z):
+        return self.decoder(z)
