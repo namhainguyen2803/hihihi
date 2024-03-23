e60fba64310ced36865741a6eb2648eb9351b24e
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2024-03-23 15:15:10 +0700

    hhh

--- dataloader/longtailed.py
+++ dataloader/longtailed.py
@@ -2,7 +2,8 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 import numpy as np
-
+from PIL import Image
+from typing import Any, Tuple
 
 class IMBALANCECIFAR10(torchvision.datasets.CIFAR10):
     cls_num = 10
@@ -79,7 +80,6 @@ class IMBALANCECIFAR100(IMBALANCECIFAR10):
     }
     cls_num = 100
 
-
 class IMBALANCEMNIST(torchvision.datasets.MNIST):
     cls_num = 10
 
@@ -89,23 +89,42 @@ class IMBALANCEMNIST(torchvision.datasets.MNIST):
         super(IMBALANCEMNIST, self).__init__(root, train, transform, target_transform, download)
         np.random.seed(rand_number)
         img_num_list = self.get_img_num_per_cls(self.cls_num, imb_type, imb_factor)
-        print(f"Number of images per class: {img_num_list}")
+        if train == True:
+            print(f"In setting of training, number of images per class: {img_num_list}")
+        else:
+            print(f"In setting of testing, number of images per class: {img_num_list}")
         self.gen_imbalanced_data(img_num_list)
 
+    def __getitem__(self, index: int) -> Tuple[Any, Any]:
+
+        img, target = self.data[index], int(self.targets[index])
+        img = Image.fromarray(img.numpy())
+
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img, target
+
     def get_img_num_per_cls(self, cls_num, imb_type, imb_factor):
-        img_max = len(self.data) / cls_num
         img_num_per_cls = []
         if imb_type == 'exp':
             for cls_idx in range(cls_num):
+                img_max = torch.sum(self.targets == cls_idx)
                 num = img_max * (imb_factor ** (cls_idx / (cls_num - 1.0)))
                 img_num_per_cls.append(int(num))
         elif imb_type == 'step':
             for cls_idx in range(cls_num // 2):
+                img_max = torch.sum(self.targets == cls_idx)
                 img_num_per_cls.append(int(img_max))
             for cls_idx in range(cls_num // 2):
+                img_max = torch.sum(self.targets == cls_idx)
                 img_num_per_cls.append(int(img_max * imb_factor))
         else:
-            img_num_per_cls.extend([int(img_max)] * cls_num)
+            raise NotImplementedError
+
         return img_num_per_cls
 
     def gen_imbalanced_data(self, img_num_per_cls):
@@ -113,16 +132,15 @@ class IMBALANCEMNIST(torchvision.datasets.MNIST):
         new_targets = []
         targets_np = np.array(self.targets, dtype=np.int64)
         classes = np.unique(targets_np)
-        # np.random.shuffle(classes)
         self.num_per_cls_dict = dict()
         for the_class, the_img_num in zip(classes, img_num_per_cls):
             self.num_per_cls_dict[the_class] = the_img_num
             idx = np.where(targets_np == the_class)[0]
-            np.random.shuffle(idx)
+            np.random.permutation(idx)
             selec_idx = idx[:the_img_num]
             new_data.append(self.data[selec_idx, ...])
             new_targets.extend([the_class, ] * the_img_num)
-        new_data = np.vstack(new_data)
+        new_data = torch.vstack(new_data)
         self.data = new_data
         self.targets = new_targets
 
