880d657afb47e05dd68e970471a4dcd1f4e0aa65
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2024-03-30 01:45:53 +0700

    hhh

--- mnist_trainer.py
+++ mnist_trainer.py
@@ -29,7 +29,7 @@ def main():
     parser.add_argument('--batch-size', type=int, default=500, metavar='N',
                         help='input batch size for training (default: 500)')
 
-    parser.add_argument('--epochs', type=int, default=50, metavar='N',
+    parser.add_argument('--epochs', type=int, default=200, metavar='N',
                         help='number of epochs to train (default: 30)')
     parser.add_argument('--lr', type=float, default=0.0001, metavar='LR',
                         help='learning rate (default: 0.0001)')
