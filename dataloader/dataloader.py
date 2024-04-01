2ac05975d7c45e7d09de875455f2017689ed3d16
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2024-04-01 21:50:01 +0700

    hhh

--- dataloader/dataloader.py
+++ dataloader/dataloader.py
@@ -4,17 +4,19 @@ import torch
 from torch.utils.data import DataLoader
 from dataloader.longtailed import IMBALANCECIFAR10, IMBALANCECIFAR100, IMBALANCEMNIST
 
+
 class BaseLTDataLoader:
-    def __init__(self, data_dir="data/", train_batch_size=250, test_batch_size=250):
+    def __init__(self, data_dir="data/", train_batch_size=250, test_batch_size=250, num_classes=0):
         self.test_loader = None
         self.train_loader = None
         self.train_dataset = None
         self.test_dataset = None
-        self.num_classes = 0
+        self.num_classes = num_classes
         self.data_dir = data_dir
         self.train_batch_size = train_batch_size
         self.test_batch_size = test_batch_size
         self.create_dataset()
+
     def create_dataset(self):
         pass
 
@@ -59,8 +61,10 @@ class BaseLTDataLoader:
 
 class MNISTLTDataLoader(BaseLTDataLoader):
     def __init__(self, data_dir="data/", train_batch_size=250, test_batch_size=250):
-        self.num_classes = 10
-        super(MNISTLTDataLoader, self).__init__(data_dir, train_batch_size, test_batch_size)
+        super(MNISTLTDataLoader, self).__init__(data_dir=data_dir,
+                                                train_batch_size=train_batch_size,
+                                                test_batch_size=test_batch_size,
+                                                num_classes=10)
 
     def create_dataset(self):
         train_set = IMBALANCEMNIST(self.data_dir + "train/",
@@ -79,13 +83,15 @@ class MNISTLTDataLoader(BaseLTDataLoader):
                                   ]))
         self.test_dataset = test_set
 
+
 class CIFAR10LTDataLoader(BaseLTDataLoader):
     def __init__(self, data_dir="data/", train_batch_size=80, test_batch_size=80):
-        self.num_classes = 10
-        super(CIFAR10LTDataLoader, self).__init__(data_dir, train_batch_size, test_batch_size)
+        super(CIFAR10LTDataLoader, self).__init__(data_dir=data_dir,
+                                                  train_batch_size=train_batch_size,
+                                                  test_batch_size=test_batch_size,
+                                                  num_classes=10)
 
     def create_dataset(self):
-
         train_set = IMBALANCECIFAR10(root=self.data_dir + "train/",
                                      imb_type='exp', imb_factor=0.05,
                                      train=True, download=True,
@@ -108,26 +114,27 @@ class CIFAR10LTDataLoader(BaseLTDataLoader):
 
 class CIFAR100LTDataLoader(BaseLTDataLoader):
     def __init__(self, data_dir="data/", train_batch_size=80, test_batch_size=80):
-        self.num_classes = 100
-        super(CIFAR100LTDataLoader, self).__init__(data_dir, train_batch_size, test_batch_size)
+        super(CIFAR100LTDataLoader, self).__init__(data_dir=data_dir,
+                                                   train_batch_size=train_batch_size,
+                                                   test_batch_size=test_batch_size,
+                                                   num_classes=100)
 
     def create_dataset(self):
-
         train_set = IMBALANCECIFAR100(root=self.data_dir + "train/",
-                                     imb_type='exp', imb_factor=0.05,
-                                     train=True, download=True,
-                                     transform=transforms.Compose([
-                                         transforms.RandomCrop(32, padding=4),
-                                         transforms.RandomHorizontalFlip(),
-                                         transforms.ToTensor(),
-                                     ]))
+                                      imb_type='exp', imb_factor=0.05,
+                                      train=True, download=True,
+                                      transform=transforms.Compose([
+                                          transforms.RandomCrop(32, padding=4),
+                                          transforms.RandomHorizontalFlip(),
+                                          transforms.ToTensor(),
+                                      ]))
 
         test_set = IMBALANCECIFAR100(self.data_dir + "test/",
-                                    imb_type='exp', imb_factor=1,
-                                    train=False, download=True,
-                                    transform=transforms.Compose([
-                                        transforms.ToTensor()
-                                    ]))
+                                     imb_type='exp', imb_factor=1,
+                                     train=False, download=True,
+                                     transform=transforms.Compose([
+                                         transforms.ToTensor()
+                                     ]))
 
         self.train_dataset = train_set
         self.test_dataset = test_set
