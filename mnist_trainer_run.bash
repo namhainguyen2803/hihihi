2b0afcb661f294bf3d58c81bfb4d9b1e774fd060
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2024-04-08 10:21:59 +0700

    hhh

--- mnist_trainer_run.bash
+++ mnist_trainer_run.bash
@@ -4,5 +4,5 @@ methods=(EFBSW FBSW lowerboundFBSW BSW None)
 
 for method in "${methods[@]}"
 do
-    python3 mnist_trainer.py --lr 0.001 --epochs 10000 --log-epoch-interval 10 --dataset mnist --datadir data --outdir result --optimizer adam --weight_swd 8 --weight_fsw 0.3 --method $method
+    python3 mnist_trainer.py --lr 0.0001 --epochs 2000 --log-epoch-interval 10 --dataset mnist --datadir data --outdir result --optimizer adam --weight_swd 8 --weight_fsw 0.3 --method $method
 done
