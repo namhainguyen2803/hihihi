7b13320a4b65b0c53904b464c35a2907f9032c89
namhainguyen2803 <hai.nn214894@sis.hust.edu.vn>
2024-04-09 22:14:23 +0700

    hhh

--- evaluate/eval_fid.py
+++ evaluate/eval_fid.py
@@ -83,6 +83,7 @@ def ultimate_evaluate_fid(args,
                                                  device=device).cpu()
         # print(tensor_generated_images)
         print(tensor_real_images.shape, tensor_decoded_images.shape, tensor_generated_images.shape)
+        print(tensor_generated_images)
         npz_real_images = create_compression_file(tensor_real_images, "statistic/real_images.npz")
         # npz_generated_images = create_compression_file(tensor_generated_images, "statistic/generated_images.npz")
 
